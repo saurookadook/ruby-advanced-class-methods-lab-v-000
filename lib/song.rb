dffca475d8e402febf33da5d2a60ecba41a00c7b
Andy Maskiell <maskiella@gmail.com>
2017-12-30 14:58:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,7 +59,6 @@ class Song
     filename_array = filename.chomp(".mp3").split(" - ")
     new_song.artist_name = filename_array[0]
     new_song.name = filename_array[1]
-    binding.pry
     new_song.save
     new_song
   end
